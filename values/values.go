e0907702f354dc57d38fc94726e0340c2f678f89
violin <357958137@qq.com>
2023-11-03 10:40:57 +0800

    字典类型

--- values/values.go
+++ values/values.go
@@ -1,214 +1,84 @@
 package values
 
 import (
-	"strconv"
-	"strings"
-
 	"github.com/bytedance/sonic"
+	"github.com/byteweap/pkg/cast"
 )
 
-// Value 值类型
-// type Value interface{}
+// 字典类型
+type ValueMap map[string]any
 
-// Value 任何类型
-type Value interface{}
+type Value any
 
-// // NewValueFromJSON 从 JSON 解析值
-// func NewValueFromJSON(data []byte) (Value, error) {
-// 	var value Value
-// 	err := json.Unmarshal(data, &value)
-// 	return value, err
-// }
-
-// NewValueMapArray 返回 ValueＭap 数组
+// NewValueMapArray creates a new array of ValueMap objects from a byte array.
+//
+// It takes a byte array as input and returns an array of ValueMap objects and an error.
 func NewValueMapArray(data []byte) ([]ValueMap, error) {
-	var values []interface{}
+	if len(data) == 0 {
+		return []ValueMap{}, nil
+	}
+	var values []ValueMap
 	err := sonic.Unmarshal(data, &values)
-	if err != nil {
-		return nil, err
-	}
-	var vms []ValueMap
-	for _, item := range values {
-		vms = append(vms, item.(map[string]interface{}))
-	}
-	return vms, nil
+	return values, err
 }
 
-// ValueMap 字典类型
-type ValueMap map[string]interface{}
-
-// NewValuesFromJSON 创建新数据
+// NewValuesFromJSON creates a new ValueMap from JSON data.
+//
+// It takes a byte slice of JSON data as a parameter and returns a ValueMap and an error.
 func NewValuesFromJSON(data []byte) (ValueMap, error) {
+	if len(data) == 0 {
+		return ValueMap{}, nil
+	}
 	var vm ValueMap
 	err := sonic.Unmarshal(data, &vm)
 	return vm, err
 }
 
-// GetValueMap 返回 ValueMap
+// GetValueMap returns a ValueMap for the given name.
 func (vm ValueMap) GetValueMap(name string) ValueMap {
 	value := vm[name]
-	// fmt.Println(reflect.TypeOf(value))
-	switch v := value.(type) {
-	case map[string]interface{}:
-		return value.(map[string]interface{})
-	case ValueMap:
-		return value.(ValueMap)
-	case string:
-		vm, _ := NewValuesFromJSON([]byte(v))
-		return vm
-	}
-	return nil
+	return cast.ToStringMap(value)
 }
 
-// GetString 获取字符串数据
+// GetString returns the string value associated with the given name in the ValueMap.
 func (vm ValueMap) GetString(name string) string {
 	value := vm[name]
-	if value == nil {
-		return ""
-	}
-	switch value.(type) {
-	case int:
-		return strconv.Itoa(value.(int))
-	case string:
-		return strings.TrimSpace(value.(string))
-	case []interface{}:
-		data, _ := sonic.Marshal(value)
-		return string(data)
-	case interface{}:
-		data, _ := sonic.Marshal(value)
-		return string(data)
-	}
-	return ""
+	return cast.ToString(value)
 }
 
-// GetInt 获取 int 类型数据
+// GetInt returns the integer value associated with the given name in the ValueMap.
 func (vm ValueMap) GetInt(name string) int {
 	value := vm[name]
-	if value == nil {
-		return 0
-	}
-	// fmt.Println(reflect.TypeOf(value))
-	switch value.(type) {
-	case int:
-		return value.(int)
-	case float64:
-		return int(value.(float64))
-	case string:
-		ret, err := strconv.Atoi(value.(string))
-		if err != nil {
-			return 0
-		}
-		return ret
-	}
-	return 0
+	return cast.ToInt(value)
 }
 
-// GetInt64 获取 int 类型数据
+// GetInt64 returns the int64 value associated with the given name in the ValueMap.
 func (vm ValueMap) GetInt64(name string) int64 {
 	value := vm[name]
-	if value == nil {
-		return 0
-	}
-	// fmt.Println(reflect.TypeOf(value))
-	switch value.(type) {
-	case int:
-		return int64(value.(int))
-	case int64:
-		return value.(int64)
-	case float64:
-		return int64(value.(float64))
-	case string:
-		ret, err := strconv.Atoi(value.(string))
-		if err != nil {
-			return 0
-		}
-		return int64(ret)
-	}
-	return 0
+	return cast.ToInt64(value)
 }
 
+// GetIntArray returns an array of integers from the given key in the ValueMap.
 func (vm ValueMap) GetIntArray(key string) []int {
-	value, ok := vm[key]
-	if !ok {
-		return []int{}
-	}
-	switch v := value.(type) {
-	case []int:
-		return v
-	case []int32:
-		ret := make([]int, len(v))
-		for i := 0; i < len(v); i++ {
-			ret[i] = int(v[i])
-		}
-		return ret
-	case []int64:
-		ret := make([]int, len(v))
-		for i := 0; i < len(v); i++ {
-			ret[i] = int(v[i])
-		}
-		return ret
-	case []interface{}:
-		bs, err := sonic.Marshal(v)
-		if err != nil {
-			return []int{}
-		}
-		arr := make([]int, 0)
-		err = sonic.Unmarshal(bs, &arr)
-		if err != nil {
-			return []int{}
-		}
-		return arr
-	case string:
-		ret := make([]int, 0)
-		_ = sonic.Unmarshal([]byte(v), &ret)
-		return ret
-	default:
-		return []int{}
-	}
-
+	value := vm[key]
+	return cast.ToIntSlice(value)
 }
 
-// GetFloat64 获取 float64 数据
+// GetFloat64 returns the float64 value associated with the given name in the ValueMap.
 func (vm ValueMap) GetFloat64(name string) float64 {
 	value := vm[name]
-	if value == nil {
-		return 0
-	}
-	switch value.(type) {
-	case float64:
-		return value.(float64)
-	case string:
-		ret, err := strconv.ParseFloat(value.(string), 64)
-		if err != nil {
-			return 0
-		}
-		return ret
-	}
-	return 0
+	return cast.ToFloat64(value)
 }
 
-// GetBool 获取 bool 值
+// GetBool retrieves a boolean value from the ValueMap.
 func (vm ValueMap) GetBool(name string) bool {
 	value := vm[name]
-	if value == nil {
-		return false
-	}
-	switch value.(type) {
-	case bool:
-		return value.(bool)
-	case int:
-		return value.(int) == 1
-	case string:
-		return value.(string) == "true"
-	}
-	return false
+	return cast.ToBool(value)
 }
 
-// // SetString 设置数据
+// ToJSON converts the ValueMap to a JSON byte slice.
 //
-//	func (f Values) SetString(name, value string) {
-//		f[name] = value
-//	}
+// It returns a byte slice containing the JSON representation of the ValueMap.
 func (vm ValueMap) ToJSON() []byte {
 	ret, _ := sonic.Marshal(vm)
 	return ret
--- values/values_test.go
+++ values/values_test.go
@@ -1,13 +1,33 @@
 package values
 
-import "testing"
+import (
+	"testing"
 
-func TestGetIntArray(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
-	vm := ValueMap{
-		"sucUsers": []int{1, 2, 3},
+func TestNewValueMapArray(t *testing.T) {
+	// Test case 1: Empty byte array
+	data := []byte{}
+	expected := []ValueMap{}
+	result, err := NewValueMapArray(data)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, result, expected)
 	}
 
-	t.Logf("v: %v", vm)
-	t.Logf("aaa: %v", vm.GetIntArray("sucUsers"))
+	// Test case 2: Byte array with valid JSON
+	data = []byte(`[{"key": "value"}]`)
+	expected = []ValueMap{{"key": "value"}}
+	result, err = NewValueMapArray(data)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, result, expected)
+	}
+
+	// Test case 3: Byte array with invalid JSON
+	data = []byte(`invalid json`)
+	_, err = NewValueMapArray(data)
+	assert.Error(t, err)
+
 }
